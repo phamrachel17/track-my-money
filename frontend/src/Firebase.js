06afc7121724a3b7c4f0afa5c07068d46de37644
Divy Gobiraj <divygobi@gmail.com>
2023-08-21 18:27:48 -0700

    added firebase.js

---
+++ frontend/src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6fzrQxsQDfyyL395BfI2rIT5HGphXO98",
+  authDomain: "track-my-80cd3.firebaseapp.com",
+  projectId: "track-my-80cd3",
+  storageBucket: "track-my-80cd3.appspot.com",
+  messagingSenderId: "355723062910",
+  appId: "1:355723062910:web:254b5ce703f460cce9722f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
